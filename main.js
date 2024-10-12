da415a09e627de7201b542691300e72a56e8a429
Akingbogun Ololade <73138104+zololade@users.noreply.github.com>
2024-10-12 21:53:14 +0100

    feat: Implement responsive design and interactive share functionality
    This commit introduces significant enhancements to both the functionality and visual appeal of the application.  The design is now fully responsive, ensuring optimal viewing experience across a range of devices.  Additionally, a dynamic share feature has been incorporated, allowing users to seamlessly interact with social sharing options.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const btn = document.querySelectorAll(".share");
 
+const profile = document.querySelector(".user__profile");
+const social = document.querySelector(".user__social");
+const bgActive = document.querySelector(".user");
+
+for (let i = 0; i <= btn.length - 1; i++) {
+  btn[i].addEventListener("click", () => {
+    profile.classList.toggle("inactive");
+    social.classList.toggle("inactive");
+    bgActive.classList.toggle("bgActive");
+  });
+}
