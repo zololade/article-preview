d5e7131483dfc362361c3337acd5cf9b9d59b35c
Akingbogun Ololade <73138104+zololade@users.noreply.github.com>
2024-10-13 18:41:00 +0100

    feat: Enhance social sharing component behavior on mobile
    Refactors the social sharing component to improve its behavior on mobile devices.
    
    The updated design ensures a cleaner user experience by displaying the social sharing options in a slide-out drawer format on mobile screens, while maintaining a static layout on larger screens.

--- main.js
+++ main.js
@@ -2,12 +2,23 @@ const btn = document.querySelectorAll(".share");
 
 const profile = document.querySelector(".user__profile");
 const social = document.querySelector(".user__social");
-const bgActive = document.querySelector(".user");
+const socialBtn = document.querySelector(".social__share");
+const profileBtn = document.querySelector(".profile__share");
 
 for (let i = 0; i <= btn.length - 1; i++) {
   btn[i].addEventListener("click", () => {
-    profile.classList.toggle("inactive");
-    social.classList.toggle("inactive");
-    bgActive.classList.toggle("bgActive");
+    profile.classList.toggle("profileInactive");
+    social.classList.toggle("socialInactive");
+    social.classList.toggle("bgActive");
+    socialBtn.classList.toggle("btnActive");
   });
 }
+
+document.addEventListener("click", (e) => {
+  if (!social.contains(e.target) && !profileBtn.contains(e.target)) {
+    social.classList.add("socialInactive");
+    social.classList.remove("bgActive");
+    profile.classList.remove("profileInactive");
+    socialBtn.classList.remove("btnActive");
+  }
+});
